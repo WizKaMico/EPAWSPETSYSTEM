b00404d7e414329893f1fdaa0c4f236dfaa0bbb0
unknown <tricore012@gmail.com>
2022-05-21 12:15:43 +0800

    W

--- ADMIN/activity/update_appointment.php
+++ ADMIN/activity/update_appointment.php
@@ -7,9 +7,9 @@ error_reporting(E_ALL);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-
-require 'vendor/PHPMailer/src/PHPMailer.php';
-require 'vendor/PHPMailer/src/SMTP.php';
+// require 'vendor/PHPMailer/src/Exception.php';
+// require 'vendor/PHPMailer/src/PHPMailer.php';
+// require 'vendor/PHPMailer/src/SMTP.php';
 
 include '../../CONNECTION/conn.php';
 
